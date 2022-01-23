25ea52586bb7627db5489e6512c750df4889a4bb
Kristin Baumann <mail@kristin-baumann.com>
2022-01-23 12:01:27 +0100

    Added share image

--- website/src/components/HelmetComponent.js
+++ website/src/components/HelmetComponent.js
@@ -1,18 +1,21 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 
+import shareImage from "../images/share_image_1.jpg";
+
 const HelmetComponent = () => (
   <Helmet>
     <meta charSet="utf-8" />
     <title>Freelancers and Employees in the Data Vis Industry</title>
     <link
       rel="canonical"
-      href="https://datavis-freelancer-employees.kristin-baumann.com/"
+      href="https://datavis-freelancers-employees.kristin-baumann.com/"
     />
     <meta
       name="description"
       content="Comparison of Freelancers and Employees in the Data Vis Society Survey 2021"
-    ></meta>
+    />
+    <meta property="og:image" content={shareImage} />
   </Helmet>
 );
 
